d7ec8407a2893af2fd19dba30fc47cdb284b6f5d
Gabriela Gonzalez <ggonzalez9313@gmail.com>
2025-07-03 18:05:24 -0400

    Added new project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,17 +4,22 @@ import Project from './Project';
 const Projects = () => {
   const projectList = [
     {
-      title: 'Swift iOS App',
+      title: '⚾Baseball Dashboard',
+      description: 'Built a Baseball Dashboard to showcase player data, team standing, and information about their team schedule.',
+      link: 'https://baseballdashboard.netlify.app/'
+    },
+    {
+      title: '📱Swift iOS App',
       description: 'Built an iOS application using Swift language. The app was a prototype non-functional MLB live score app.',
       link: 'https://youtube.com/shorts/T9F9lNfp9mU?feature=share'
     },
     {
-      title: '2D Platformer Video Game',
+      title: '🎮2D Platformer Video Game',
       description: 'Having a passion for gaming, I built a 2D platformer game using Java. Watch the Youtube Playlist to see gameplay and see how it was built!',
       link: 'https://github.com/johndoe/project-two'
     },
     {
-      title: 'This Website',
+      title: '💻This Website',
       description: 'Built this website to host my projects using HTML, CSS, JS, and React!',
     }
   ];
